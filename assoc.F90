a9ee24cf4fef7fd04f77e803839d1a362290f899
Michael Levy <mike.levy.work@gmail.com>
2019-08-02 18:58:15 -0600

    Add README highlighting issue(s)
    Updated assoc.F90 to include a derived type that behaves like the problematic
    section of MARBL. The good news is that it looks like the derived-type bug was
    fixed in gfortran 5.5; the bad news is that the associate statement of an array
    slice looks to still be broken?

--- assoc.F90
+++ assoc.F90
@@ -1,25 +1,51 @@
 Program assoc
 
-  real, dimension(3,3) :: mat
+  type :: test_type
+    integer :: a
+    integer :: b
+    integer :: c
+  end type test_type
+
+  type(test_type) :: mat_type(3)
+  integer, dimension(3,3) :: mat
   integer :: i, j
 
   do j=1,3
+    mat_type(j)%a = j-1
+    mat_type(j)%b = 10 + j-1
+    mat_type(j)%c = 20 + j-1
+
     do i=1,3
-      mat(i,j) = real(10*(i-1) + j-1)
+      mat(i,j) = 10*(i-1) + j-1
     end do
   end do
 
+  ! (1) basic version of bug
   associate(col => mat(1,:))
+    print*, 'print a column of a matrix'
     print*, mat(1,:)
+    print*, 'pass associated column to function expecting dimension(:)'
     call print_vec_colon(col)
+    print*, 'pass associated column to function expecting dimension(3)'
     call print_vec_num(col)
   end associate
+
+  associate(col => mat_type(:)%a)
+    print*, 'print an array of derived-type elements'
+    print*, mat_type(:)%a
+    print*, 'pass associated column to function expecting dimension(:)'
+    call print_vec_colon(col)
+    print*, 'pass associated column to function expecting dimension(3)'
+    call print_vec_num(col)
+  end associate
+
+  ! (2) Attempting to mimic the problem in MARBL
 
 contains
 
   subroutine print_vec_colon(vec)
 
-    real :: vec(:)
+    integer :: vec(:)
 
     print*, vec
 
@@ -27,7 +53,7 @@ contains
 
   subroutine print_vec_num(vec)
 
-    real :: vec(3)
+    integer :: vec(3)
 
     print*, vec
 
